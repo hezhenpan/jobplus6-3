9841be083f3aeea419c4c731772c2634752d35f5
hezhenpan <hezhenpan@163.com>
2018-05-25 19:34:05 +0800

    1.更新配置

--- jobplus/config.py
+++ jobplus/config.py
@@ -4,7 +4,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:123456@localhost:3306/jobplus?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     INDEX_PER_PAGE = 9
     COMINFO_PER_PAGE = 9
